302964a570986b414e130429dba3407dfb4845bc
ereillo <evelioreillo@gmail.com>
2023-07-13 16:36:41 +0200

    done

--- js/index.js
+++ js/index.js
@@ -21,3 +21,24 @@ else {
 }
 
 // Iteration 3: Loops
+let result = " ";
+let result2= " "
+
+for (let i = 0; i < hacker1.length; i++) {
+    result += hacker1[i].toUpperCase() + " ";
+}
+console.log(result)
+
+for (let i = hacker2.length -1; i >= 0; i--) {
+    result2 += hacker2[i] + " "
+}
+
+console.log(result2)
+
+if (hacker1 < hacker2) {
+    console.log("The driver's name goes first.");
+  } else if (hacker1 > hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+  } else {
+    console.log("What?! You both have the same name?");
+  }
